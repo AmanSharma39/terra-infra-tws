2f825bed481eb4cac490ed2e3c046dd1d4448900
AmanSharma39 <156844703+AmanSharma39@users.noreply.github.com>
2024-10-19 00:01:19 +0530

    Add files via upload

---
+++ my-table.tf
@@ -0,0 +1,11 @@
+# DynamoDB table
+
+resource "aws_dynamodb_table" "my_app_table" {
+    name = "${var.my_env}-tws-table"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "userID"
+    attribute {
+        name = "userID"
+        type = "S"
+    }
+}
